0f04e247f46c6b50234a2232824fb85cc019103e
Roberto Martínez <rmroman@itesm.mx>
2017-08-15 15:27:26 -0500

    Archivos .py agregados

---
+++ auto.py
@@ -0,0 +1,7 @@
+#encoding: UTF-8
+
+# Autor: tuNombreCompleto, tuMatricula
+# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+
+# A partir de aquí escribe tu programa
+
---
+++ miInfo.py
@@ -0,0 +1,7 @@
+#encoding: UTF-8
+
+# Autor: tuNombreCompleto, tuMatricula
+# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+
+# A partir de aqui escribe tu programa
+
