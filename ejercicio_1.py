7d1639d15c6f26c55c159086a02d01b1bf5df27a
yerishvaldes <yerishvaldes@outlook.com>
2017-08-20 13:25:02 -0500

    ejercicio_1

---
+++ ejercicio_1.py
@@ -0,0 +1,6 @@
+nombre= "Yerish Alejandro Valdes Bernes"
+matricula= "A01375755"
+carrera= "Iti"
+escuela= "Prepa Tec, programa Bicultural"
+gustos="Escuchar musica y jugar videojuegos"
+print( "Nombre:"+nombre+"\n"+"Matricula:"+matricula+"\n"+"Carrera:"+carrera+"\n"+"Escuela:"+escuela+"\n"+"Gustos:"+gustos)
