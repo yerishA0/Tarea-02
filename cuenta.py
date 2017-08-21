b0e9615d60882d809314de0f4266199466a431cc
yerishvaldes <yerishvaldes@outlook.com>
2017-08-20 20:52:28 -0500

    cuenta terminado

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,13 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: yerish valdes A01375755
+# Descripcion: cuenta total por una comida
 
 # A partir de aquí escribe tu programa
-
+costo=int(input("Costo de la comida:"))
+propina=costo*0.12
+print("propina:",propina)
+iva=costo*0.16
+print("IVA:",iva)
+cuenta_total=costo+propina+iva
+print("la cuenta total es: ",cuenta_total)
